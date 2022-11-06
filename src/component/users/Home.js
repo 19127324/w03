e6848ce6d52a39ff5da0e3af60625400702a8a98
19127324 <64062447+19127324@users.noreply.github.com>
2022-11-06 13:57:49 +0700

    test

--- src/component/users/Home.js
+++ src/component/users/Home.js
@@ -1,6 +1,8 @@
 function Home() {
   return <div className="container">
-    <div className="container-login">Home</div>
+    <div className="container-login">
+      <label className="text">Hello</label>
+      </div>
   </div>;
 };
 
